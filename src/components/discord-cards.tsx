ef701ee1f12875549ba4bcca5d3720aff28088ec
WayneLuvsU <engr.wayne02@gmail.com>
2026-01-07 15:27:30 +0800

    Update discord-cards.tsx

--- src/components/discord-cards.tsx
+++ src/components/discord-cards.tsx
@@ -6,7 +6,7 @@ export const DiscordCards = () => {
 
   useEffect(() => {
     const songs = [
-      "https://file.garden/aN0Uo2YmaWI-OmAY/Hev%20Abi%20-%20molly%20to%20the%20head%20freestyle.mp3",
+      "https://file.garden/aN0Uo2YmaWI-OmAY/Hev%20Abi%20-%20MEDICAL%20(1)%20(mp3cut.net).mp3",
       "https://file.garden/aN0Uo2YmaWI-OmAY/Future%20-%20WAIT%20FOR%20U%20(Official%20Audio)%20ft%20(mp3cut.net).mp3",
       "https://file.garden/aN0Uo2YmaWI-OmAY/ssstik.io_@supahflyyyy_1766001840975.mp3",
       "https://file.garden/aN0Uo2YmaWI-OmAY/NEMZZZ%20-%20COLD%20(OFFICIAL%20VIDEO).mp3",
