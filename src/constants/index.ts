1f901b662b310b46638de646c54cd3c0aa95372f
WayneLuvsU <engr.wayne02@gmail.com>
2026-08-22 23:28:00 +0800

    Add navigation items to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,18 @@
-
+export const NAV_ITEMS = [
+  {
+    label: "About",
+    href: "#about",
+  },
+  {
+    label: "Story",
+    href: "#story",
+  },
+  {
+    label: "Archives",
+    href: "#archives",
+  },
+  {
+    label: "Contact",
+    href: "#contact",
+  },
+];
