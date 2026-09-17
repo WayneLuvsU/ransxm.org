b5d34dea0ed3e4b6c551440b4577dcbf5dbbc7af
WayneLuvsU <engr.wayne02@gmail.com>
2026-09-17 21:58:49 +0800

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -116,30 +116,6 @@ export const Navbar = () => {
               ))}
             </div>
 
-      
-            <div className="flex items-center gap-4">
-              <button
-                onClick={toggleAudioIndicator}
-                className="ml-10 flex items-center space-x-1 p-2 transition hover:opacity-75"
-                title="Play Audio"
-              >
-
-                {Array(4)
-                  .fill("")
-                  .map((_, i) => (
-                    <div
-                      key={i + 1}
-                      className={cn(
-                        "indicator-line",
-                        isIndicatorActive && "active"
-                      )}
-                      style={{
-                        animationDelay: `${(i + 1) * 0.1}s`,
-                      }}
-                    />
-                  ))}
-              </button>
-            </div>
 
           </div>
         </nav>
